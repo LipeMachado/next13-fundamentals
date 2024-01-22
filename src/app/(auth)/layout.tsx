cae470dc4d40ce8f97ec6daf9aab623d7d2867d3
LipeMachado <silvalipe765@gmail.com>
2024-01-22 17:43:18 -0300

    Update

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,8 +3,8 @@ import { ReactNode } from 'react'
 export default function AdminLayout({ children }: { children: ReactNode }) {
     return (
         <>
+            <h1 className='text-xl font-bold'>Logo do app</h1>
             <div>{children}</div>
-            <div>Layout Admin</div>
         </>
     )
 }
